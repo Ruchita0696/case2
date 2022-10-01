016b02f8665e843d9a1b4af913224412a7df4737
Ruchita0696 <ruchitapatil045@gmail.com>
2022-10-01 21:07:20 +0530

    first commit

---
+++ forloop.sh
@@ -0,0 +1,7 @@
+Power=1
+read -p "Enter an argument:" n
+for (( num=1; num<=n; num++ ))
+do
+	Power=$(( Power * 2 ))
+	echo $Power
+done
---
+++ harmonic.sh
@@ -0,0 +1,7 @@
+read -p "Enter term num: " n
+First_Term=0
+for (( num=1; num<=n; num++ ))
+do
+	First_Term=$(( First_Term + 1 ))
+done
+echo "1/$First_Term"
